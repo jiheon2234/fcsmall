10fd4b88b57ee0588a8f049592deac97302556e8
infinity <88617484+Exiinfinity@users.noreply.github.com>
2023-10-03 19:58:49 +0900

    vite.config.js 설정

--- frontend/vue-shoppingmall/vite.config.ts
+++ frontend/vue-shoppingmall/vite.config.ts
@@ -14,5 +14,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    outDir: "../src/main/resources",
+  },
+  // server: {
+  //   "/api": "http://localhost:8080",
+  // }
 })
